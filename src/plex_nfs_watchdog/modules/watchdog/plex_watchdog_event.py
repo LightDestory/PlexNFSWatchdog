bef23641778bf3f6e4d25460a8dc398a37c7ba03
Alessio Tudisco <apb231@gmail.com>
2023-04-25 14:59:09 +0200

    fix: file inotify not handled

--- src/plex_nfs_watchdog/modules/watchdog/plex_watchdog_event.py
+++ src/plex_nfs_watchdog/modules/watchdog/plex_watchdog_event.py
@@ -6,6 +6,4 @@ from ..plex.plex_agent import plex_agent_singleton
 class PlexWatchdog(FileSystemEventHandler):
 
     def on_any_event(self, event):
-        if not event.is_directory:
-            return
         plex_agent_singleton.parse_event(event)
