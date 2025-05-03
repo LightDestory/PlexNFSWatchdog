373fb2c01b1491a9b9891d7a25a6dd7ee2aca3ba
LightDestory <apb231@gmail.com>
2025-05-03 22:55:34 +0200

    fix: SECTION KEY INSTEAD OF UUID

--- src/plex_nfs_watchdog/modules/plex/plex_agent.py
+++ src/plex_nfs_watchdog/modules/plex/plex_agent.py
@@ -81,15 +81,15 @@ class PlexAgent:
                 return True
         return False
 
-    def _get_uuid_from_root_folder(self, root_folder: str) -> str:
+    def _get_id_from_root_folder(self, root_folder: str) -> str:
         """
-        Retrieves the UUID of a section from one root folder name
+        Retrieves the ID of a section from one root folder name
         :param root_folder: A root folder name
-        :return: The UUID of the section, raises ValueError if not found
+        :return: The ID of the section, raises ValueError if not found
         """
-        for uuid, section_data in self._internal_sections.items():
+        for id, section_data in self._internal_sections.items():
             if root_folder in section_data["root_folders"]:
-                return uuid
+                return id
         raise ValueError(f"Root folder {root_folder} not found in Plex sections")
 
     def tokenized(self) -> bool:
@@ -177,16 +177,16 @@ class PlexAgent:
     def _inspect_library(self) -> int:
         """
         Inspects the Plex library and loads the internal paths.
-        For each section, it stores the UUID, title, locations and unique root folders
+        For each section, it stores the ID, title, locations and unique root folders
         :return: The number of sections found
         """
         for section in self._server.library.sections():
-            self._internal_sections[section.uuid] = {"title": section.title, "locations": [], "root_folders": set()}
+            self._internal_sections[section.key] = {"title": section.title, "locations": [], "root_folders": set()}
             for location in section.locations:
                 tmp = Path(location)
-                if tmp not in self._internal_sections[section.uuid]["locations"]:
-                    self._internal_sections[section.uuid]["locations"].append(tmp)
-                self._internal_sections[section.uuid]["root_folders"].add(tmp.name)
+                if tmp not in self._internal_sections[section.key]["locations"]:
+                    self._internal_sections[section.key]["locations"].append(tmp)
+                self._internal_sections[section.key]["root_folders"].add(tmp.name)
                 self._fast_section_lookup.add(tmp.name)
         return len(self._internal_sections)
 
@@ -204,7 +204,7 @@ class PlexAgent:
         1. If the path is a file and the extension is not in the supported list, return None
         2. If the path is a directory, check if it is child of a Plex section
         :param path: The path to validate
-        :return: A tuple containing the section UUID and the item name if valid, None otherwise
+        :return: A tuple containing the section ID and the item name if valid, None otherwise
         """
         if shared.user_input.verbose:
             logging.info(f"Analyzing {path.absolute()}")
@@ -215,22 +215,22 @@ class PlexAgent:
         cursor = path
         while len(cursor.parents) != 0:
             if cursor.parent.name in self._fast_section_lookup:
-                uuid = self._get_uuid_from_root_folder(cursor.parent.name)
-                return uuid, cursor.name
+                id = self._get_id_from_root_folder(cursor.parent.name)
+                return id, cursor.name
             else:
                 cursor = cursor.parent
         if shared.user_input.verbose:
             logging.info(f"Path {path.absolute()} is not a child of a Plex section")
         return None
 
-    def _scan(self, section_uuid: str, item: str) -> None:
+    def _scan(self, section_id: str, item: str) -> None:
         """
         Request Plex to scan the given item in the given section
-        :param section_uuid: The UUID of the section to scan
+        :param section_id: The id of the section to scan
         :param item: The folder name to scan
         """
-        plex_section = self._server.library.sectionByID(section_uuid)
-        for location in self._internal_sections[section_uuid]["locations"]:
+        plex_section = self._server.library.sectionByID(section_id)
+        for location in self._internal_sections[section_id]["locations"]:
             scan_path: Path = Path(location / item).absolute()
             if not self._server.isBrowsable(scan_path):
                 logging.info(f"Skipping Plex scan for {str(scan_path)} because it is not browsable")
